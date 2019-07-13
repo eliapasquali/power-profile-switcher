cc7c39b48738d9ab73f0546aca7a296d08eedb86
Andrey Sitnik <andrey@sitnik.ru>
2019-07-13 14:12:49 -0400

    Remove unnecessary comments

--- extension.js
+++ extension.js
@@ -2,27 +2,9 @@ const UPower = imports.ui.status.power.UPower
 const Main = imports.ui.main
 const Lang = imports.lang
 
-/* exported init, enable, disable */
-
-// Main extension API
-function init () { }
-
-// Enable
-function enable () {
-  battery.bind().update()
-}
-
-// Disable
-function disable () {
-  battery.unbind().show()
-}
-
-// Namespace for extension logic
 let battery = {
-  // Watcher ID to disable listening
   watching: null,
 
-  // Start listen to battery status changes
   bind () {
     this.getBattery(proxy => {
       let update = Lang.bind(this, this.update)
@@ -30,28 +12,24 @@ let battery = {
     })
   },
 
-  // Stop listen to battery status changes
   unbind () {
     this.getBattery(proxy => {
       proxy.disconnect(this.watching)
     })
   },
 
-  // Show battery icon in status area
   show () {
     this.getBattery((proxy, icon) => {
       icon.show()
     })
   },
 
-  // Hide battery icon in status area
   hide () {
     this.getBattery((proxy, icon) => {
       icon.hide()
     })
   },
 
-  // Check current battery state and hide or show icon
   update () {
     this.getBattery(p => {
       if (p.Type === UPower.DeviceKind.BATTERY && p.Percentage === 100) {
@@ -62,12 +40,22 @@ let battery = {
     })
   },
 
-  // Run `callback`, only if battery is avaiable. First argument will be icon,
-  // second will be it proxy.
   getBattery (callback) {
     let menu = Main.panel.statusArea.aggregateMenu
     if (menu && menu._power) {
       callback(menu._power._proxy, menu._power.indicators)
     }
   }
+}
+
+/* exported init, enable, disable */
+
+function init () { }
+
+function enable () {
+  battery.bind().update()
+}
+
+function disable () {
+  battery.unbind().show()
 }
