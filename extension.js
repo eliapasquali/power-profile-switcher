372121947b74d617389d6228a83565a551335aaa
ennioitaliano <ennio.italiano@gmail.com>
2022-11-24 20:24:37 +0100

    first commit

--- extension.js
+++ extension.js
@@ -1,75 +1,85 @@
-const ExtensionUtils = imports.misc.extensionUtils
-const Main = imports.ui.main
-const UPower = imports.gi.UPowerGlib
+const ExtensionUtils = imports.misc.extensionUtils;
+const Main = imports.ui.main;
+const UPower = imports.gi.UPowerGlib;
+const Gio = imports.gi.Gio;
 
-let batteryWatching, settingsWatching, settings, disabled
+let batteryWatching, settingsWatching, settings, disabled;
 
-function show() {
-  getBattery((proxy, icon) => {
-    icon.show()
-  })
-}
+function switchProfile(profile) {
+    try {
+        // The process starts running immediately after this function is called. Any
+        // error thrown here will be a result of the process failing to start, not
+        // the success or failure of the process itself.
+        let proc = Gio.Subprocess.new(
+            // The program and command options are passed as a list of arguments
+            ["powerprofilesctl", "set", profile],
 
-function hide() {
-  getBattery((proxy, icon) => {
-    icon.hide()
-  })
+            // The flags control what I/O pipes are opened and how they are directed
+            Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
+        );
+
+        // Once the process has started, you can end it with `force_exit()`
+        //proc.force_exit();
+    } catch (e) {
+        logError(e);
+    }
 }
 
 function update() {
-  let hideOn = settings.get_int('hide-on')
-  getBattery(proxy => {
-    let isDischarging = proxy.State === UPower.DeviceState.DISCHARGING
-    let isFullyCharged = proxy.State === UPower.DeviceState.FULLY_CHARGED
-    if (proxy.Type !== UPower.DeviceKind.BATTERY) {
-      show()
-    } else if (isFullyCharged) {
-      hide()
-    } else if (proxy.Percentage >= hideOn && !isDischarging) {
-      hide()
-    } else {
-      show()
-    }
-  })
+    let chosenpercentage = settings.get_int("chosenpercentage");
+    getBattery((proxy) => {
+        console.log("percentuale: " + proxy.Percentage);
+        let isDischarging = proxy.State === UPower.DeviceState.DISCHARGING;
+        if (!isDischarging) {
+            switchProfile("performance");
+        } else if (proxy.Percentage >= chosenpercentage && isDischarging) {
+            switchProfile("balanced");
+        } else if (proxy.Percentage < chosenpercentage && isDischarging) {
+            switchProfile("power-saver");
+        }
+    });
 }
 
 function getBattery(callback) {
-  if (Main.panel.statusArea.quickSettings) {
-    let system = Main.panel.statusArea.quickSettings._system
-    if (system._systemItem._powerToggle) {
-      callback(system._systemItem._powerToggle._proxy, system)
+    if (Main.panel.statusArea.quickSettings) {
+        let system = Main.panel.statusArea.quickSettings._system;
+        if (system._systemItem._powerToggle) {
+            callback(system._systemItem._powerToggle._proxy, system);
+        }
+    } else {
+        let menu = Main.panel.statusArea.aggregateMenu;
+        if (menu && menu._power) {
+            callback(menu._power._proxy, menu._power);
+        }
     }
-  } else {
-    let menu = Main.panel.statusArea.aggregateMenu
-    if (menu && menu._power) {
-      callback(menu._power._proxy, menu._power)
-    }
-  }
 }
 
 function init() {
-  disabled = true
+    disabled = true;
 }
 
 function enable() {
-  if (disabled) {
-    disabled = false
-    settings = ExtensionUtils.getSettings('ru.sitnik.autohide-battery')
-    settingsWatching = settings.connect('changed::hide-on', update)
-    getBattery(proxy => {
-      batteryWatching = proxy.connect('g-properties-changed', update)
-    })
-    update()
-  }
+    if (disabled) {
+        disabled = false;
+        settings = ExtensionUtils.getSettings("ennioitaliano.power-profile-switcher");
+        settingsWatching = settings.connect(
+            "changed::chosenpercentage",
+            update
+        );
+        getBattery((proxy) => {
+            batteryWatching = proxy.connect("g-properties-changed", update);
+        });
+        update();
+    }
 }
 
 function disable() {
-  if (Main.sessionMode.currentMode !== 'unlock-dialog') {
-    disabled = true
-    if (settings) settings.disconnect(settingsWatching)
-    getBattery(proxy => {
-      proxy.disconnect(batteryWatching)
-    })
-    show()
-  }
+    if (Main.sessionMode.currentMode !== "unlock-dialog") {
+        disabled = true;
+        if (settings) settings.disconnect(settingsWatching);
+        getBattery((proxy) => {
+            proxy.disconnect(batteryWatching);
+        });
+        switchProfile();
+    }
 }
--- prefs.js
+++ prefs.js
@@ -1,51 +1,44 @@
-const ExtensionUtils = imports.misc.extensionUtils
-const Gettext = imports.gettext
-const Gtk = imports.gi.Gtk
+const ExtensionUtils = imports.misc.extensionUtils;
+const Gettext = imports.gettext;
+const Gtk = imports.gi.Gtk;
 
-let _ = Gettext.domain('autohide-battery').gettext
+let _ = Gettext.domain("power-profile-switcher").gettext;
 
 function init() {
-  ExtensionUtils.initTranslations('autohide-battery')
+    ExtensionUtils.initTranslations("power-profile-switcher");
 }
 
 function buildPrefsWidget() {
-  let settings = ExtensionUtils.getSettings('ru.sitnik.autohide-battery')
+    let settings = ExtensionUtils.getSettings("ennioitaliano.power-profile-switcher");
 
-  let grid = new Gtk.Grid({
-    margin_top: 24,
-    margin_bottom: 24,
-    margin_start: 24,
-    margin_end: 24,
-    column_spacing: 24,
-    row_spacing: 12,
-    halign: Gtk.Align.CENTER
-  })
+    let grid = new Gtk.Grid({
+        margin_top: 24,
+        margin_bottom: 24,
+        margin_start: 24,
+        margin_end: 24,
+        column_spacing: 24,
+        row_spacing: 12,
+        halign: Gtk.Align.CENTER,
+    });
 
-  let label = new Gtk.Label({
-    label: _('Hide on battery level above'),
-    halign: Gtk.Align.START
-  })
-  grid.attach(label, 0, 0, 1, 1)
+    let label = new Gtk.Label({
+        label: _("Switch between balanced and battery saver when the battery drops under the above percentage"),
+        halign: Gtk.Align.START,
+    });
+    grid.attach(label, 0, 0, 1, 1);
 
-  let field = new Gtk.SpinButton()
-  field.set_range(0, 100)
-  field.set_sensitive(true)
-  field.set_increments(1, 10)
-  grid.attach(field, 1, 0, 1, 1)
+    let field = new Gtk.SpinButton();
+    field.set_range(0, 100);
+    field.set_sensitive(true);
+    field.set_increments(1, 10);
+    grid.attach(field, 1, 0, 1, 1);
 
-  let note = new Gtk.Label({
-    label: _('If you changed maximum charging level to extend battery life'),
-    halign: Gtk.Align.CENTER
-  })
-  note.get_style_context().add_class('dim-label')
-  grid.attach(note, 0, 1, 2, 1)
-
-  field.set_value(settings.get_int('hide-on'))
-  field.connect('value-changed', widget => {
-    settings.set_int('hide-on', widget.get_value_as_int())
-  })
-  settings.connect('changed::hide-on', () => {
-    field.set_value(settings.get_int('hide-on'))
-  })
-  return grid
+    field.set_value(settings.get_int("chosenpercentage"));
+    field.connect("value-changed", (widget) => {
+        settings.set_int("chosenpercentage", widget.get_value_as_int());
+    });
+    settings.connect("changed::chosenpercentage", () => {
+        field.set_value(settings.get_int("chosenpercentage"));
+    });
+    return grid;
 }
