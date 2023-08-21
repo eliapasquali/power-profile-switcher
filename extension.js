247e37c29adcb222348f55ad40a684aa919675a0
oscfdezdz <42654671+oscfdezdz@users.noreply.github.com>
2023-08-21 16:18:33 +0200

    Port extension to ESM

--- extension.js
+++ extension.js
@@ -1,9 +1,10 @@
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const { loadInterfaceXML } = imports.misc.fileUtils;
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import UPower from 'gi://UPowerGlib';
 
-const Main = imports.ui.main;
-const { Gio, GLib, UPowerGlib:UPower } = imports.gi;
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as FileUtils from 'resource:///org/gnome/shell/misc/fileUtils.js';
 
 let settings, client, device;
 
@@ -36,7 +37,7 @@ const PowerManagerProxy = Gio.DBusProxy.makeProxyWrapper(DisplayDeviceInterface)
 const POWER_PROFILES_BUS_NAME = 'net.hadess.PowerProfiles';
 const POWER_PROFILES_OBJECT_PATH = '/net/hadess/PowerProfiles';
 
-const PowerProfilesIface = loadInterfaceXML('net.hadess.PowerProfiles');
+const PowerProfilesIface = FileUtils.loadInterfaceXML('net.hadess.PowerProfiles');
 const PowerProfilesProxy = Gio.DBusProxy.makeProxyWrapper(PowerProfilesIface);
 
 
@@ -102,109 +103,111 @@ const getDefaults = () => {
     batteryThreshold = settings.get_int("threshold");
 }
 
-function init() {
-    ExtensionUtils.initTranslations(Me.metadata.uuid);
-}
-
-function enable() {
-    client = UPower.Client.new();
-    device = client.get_display_device();
-
-    settings = ExtensionUtils.getSettings(
-        "org.gnome.shell.extensions.power-profile-switcher"
-    );
-
-    batteryPercentageWatcher = settings.connect(
-        "changed::threshold",
-        checkProfile
-    );
-    
-    ACDefaultWatcher = settings.connect(
-        "changed::ac",
-        checkProfile
-    );
-
-    batteryDefaultWatcher = settings.connect(
-        "changed::bat",
-        checkProfile
-    );
-
-    powerManagerCancellable = new Gio.Cancellable();
-    powerManagerProxy = new PowerManagerProxy(Gio.DBus.system, UPOWER_BUS_NAME, UPOWER_OBJECT_PATH,
-        (proxy, error) => {
-            if (error) {
-                logError(error.message);
-                return;
-            }
-            batteryThresholdWatcher = powerManagerProxy.connect('g-properties-changed', checkProfile);
-            checkProfile();
-        }, powerManagerCancellable);
-
-
-    powerProfilesCancellable = new Gio.Cancellable();
-    powerProfilesProxy = new PowerProfilesProxy(Gio.DBus.system, POWER_PROFILES_BUS_NAME, POWER_PROFILES_OBJECT_PATH,
-        (proxy, error) => {
-            if (error) {
-                logError(error.message);
-            } else {
-                powerProfileWatcher = powerProfilesProxy.connect('g-properties-changed', (p, properties) => {
-                    const payload = properties?.deep_unpack();
-
-                    if (payload?.ActiveProfile) {
-                        activeProfile = payload?.ActiveProfile?.unpack();
-                        if (perfDebounceTimerId) {
-                            GLib.source_remove(perfDebounceTimerId);
-                            perfDebounceTimerId = null;
-                        }
-                    }
-                    
-                    const isOnPowerSupply = device?.power_supply ||
-                        device?.state !== UPower.DeviceState.PENDING_DISCHARGE ||
-                        device?.state !== UPower.DeviceState.DISCHARGING;
-        
-                    if (isOnPowerSupply && payload?.PerformanceDegraded) {
-                        try {
-                            const reason = payload?.PerformanceDegraded?.unpack();
-
-                            if (reason === 'lap-detected') {
-                                perfDebounceTimerId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 2000, () => {
-                                    checkProfile();
-                                    perfDebounceTimerId = null;
-                                    return GLib.SOURCE_REMOVE;
-                                });
-                            }
-                            else if (reason) {
-                                log(`ActiveProfile: ${activeProfile}, PerformanceDegraded: ${reason}`);
-                            }
-                        }
-                        catch (e) {
-                            logError(e)
-                        }
-                    }
-                });
-            }
-        }, powerProfilesCancellable);
-}
-
-function disable() {
-    settings.disconnect(batteryPercentageWatcher);
-    settings.disconnect(ACDefaultWatcher);
-    settings.disconnect(batteryDefaultWatcher);
-
-    powerManagerProxy.disconnect(batteryThresholdWatcher);
-    powerManagerCancellable.cancel();
-
-    powerProfilesProxy.disconnect(powerProfileWatcher);
-    powerProfilesCancellable.cancel();
-
-    if (perfDebounceTimerId) {
-        GLib.source_remove(perfDebounceTimerId);
-        perfDebounceTimerId = null;
+export default class PowerProfileSwitcher extends Extension {
+    constructor(metadata) {
+        super(metadata);
     }
 
-    settings = null;
-    client = null;
-    device = null;
-    activeProfile = null;
-    switchProfile("balanced");
+    enable() {
+        client = UPower.Client.new();
+        device = client.get_display_device();
+
+        settings = this.getSettings(
+            "org.gnome.shell.extensions.power-profile-switcher"
+        );
+
+        batteryPercentageWatcher = settings.connect(
+            "changed::threshold",
+            checkProfile
+        );
+        
+        ACDefaultWatcher = settings.connect(
+            "changed::ac",
+            checkProfile
+        );
+
+        batteryDefaultWatcher = settings.connect(
+            "changed::bat",
+            checkProfile
+        );
+
+        powerManagerCancellable = new Gio.Cancellable();
+        powerManagerProxy = new PowerManagerProxy(Gio.DBus.system, UPOWER_BUS_NAME, UPOWER_OBJECT_PATH,
+            (proxy, error) => {
+                if (error) {
+                    logError(error.message);
+                    return;
+                }
+                batteryThresholdWatcher = powerManagerProxy.connect('g-properties-changed', checkProfile);
+                checkProfile();
+            }, powerManagerCancellable);
+
+
+        powerProfilesCancellable = new Gio.Cancellable();
+        powerProfilesProxy = new PowerProfilesProxy(Gio.DBus.system, POWER_PROFILES_BUS_NAME, POWER_PROFILES_OBJECT_PATH,
+            (proxy, error) => {
+                if (error) {
+                    logError(error.message);
+                } else {
+                    powerProfileWatcher = powerProfilesProxy.connect('g-properties-changed', (p, properties) => {
+                        const payload = properties?.deep_unpack();
+
+                        if (payload?.ActiveProfile) {
+                            activeProfile = payload?.ActiveProfile?.unpack();
+                            if (perfDebounceTimerId) {
+                                GLib.source_remove(perfDebounceTimerId);
+                                perfDebounceTimerId = null;
+                            }
+                        }
+
+                        const isOnPowerSupply = device?.power_supply ||
+                            device?.state !== UPower.DeviceState.PENDING_DISCHARGE ||
+                            device?.state !== UPower.DeviceState.DISCHARGING;
+
+                        if (isOnPowerSupply && payload?.PerformanceDegraded) {
+                            try {
+                                const reason = payload?.PerformanceDegraded?.unpack();
+
+                                if (reason === 'lap-detected') {
+                                    perfDebounceTimerId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 2000, () => {
+                                        checkProfile();
+                                        perfDebounceTimerId = null;
+                                        return GLib.SOURCE_REMOVE;
+                                    });
+                                }
+                                else if (reason) {
+                                    log(`ActiveProfile: ${activeProfile}, PerformanceDegraded: ${reason}`);
+                                }
+                            }
+                            catch (e) {
+                                logError(e)
+                            }
+                        }
+                    });
+                }
+            }, powerProfilesCancellable);
+    }
+
+    disable() {
+        settings.disconnect(batteryPercentageWatcher);
+        settings.disconnect(ACDefaultWatcher);
+        settings.disconnect(batteryDefaultWatcher);
+
+        powerManagerProxy.disconnect(batteryThresholdWatcher);
+        powerManagerCancellable.cancel();
+
+        powerProfilesProxy.disconnect(powerProfileWatcher);
+        powerProfilesCancellable.cancel();
+
+        if (perfDebounceTimerId) {
+            GLib.source_remove(perfDebounceTimerId);
+            perfDebounceTimerId = null;
+        }
+
+        settings = null;
+        client = null;
+        device = null;
+        activeProfile = null;
+        switchProfile("balanced");
+    }
 }
